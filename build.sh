342e1f541e6dc7b9dbaa7b48553fdf92ead6ae29
Yudu Ban <banyudu@innobuddy.com>
2019-05-09 17:47:39 +0800

    refactor

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 tags=$(<./tags)
 
 for tag in $tags; do
-    echo FROM node:$tag > ./Dockerfile
-    echo "RUN echo http://mirrors.aliyun.com/alpine/v3.9/main/ > /etc/apk/repositories \
-  && apk update \
-  && apk add tzdata git \
-  && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
-  && echo Asia/Shanghai > /etc/timezone" >> ./Dockerfile
-    echo RUN npm config set registry https://registry-npm.smartstudy.com/ >> ./Dockerfile
-    echo USER node >> ./Dockerfile
-    echo RUN npm config set registry https://registry-npm.smartstudy.com/ >> ./Dockerfile
-    echo "RUN cd /tmp && npm i " $(paste -s -d" "  ./packages) "&& rm -rf /tmp/*" >> ./Dockerfile
-    echo USER root >> ./Dockerfile
+    cat <<EOF > ./Dockerfile
+    FROM node:$tag
+    RUN echo http://mirrors.aliyun.com/alpine/v3.9/main/ > /etc/apk/repositories \
+      && apk update \
+      && apk add tzdata git \
+      && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
+      && echo Asia/Shanghai > /etc/timezone
+    USER node
+    ENV SASS_BINARY_SITE https://npm.taobao.org/mirrors/node-sass/
+    RUN npm config set registry https://registry-npm.smartstudy.com/
+    RUN cd /tmp && npm i $(paste -s -d" "  ./packages) && rm -rf /tmp/*
+    USER root
+EOF
     docker build . -t zhikesmartstudy/node:$tag
-    docker push zhikesmartstudy/node:$tag
+    # docker push zhikesmartstudy/node:$tag
 done
