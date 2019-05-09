eda5cef3af36194ae4f440916745d51be928dcd9
Yudu Ban <banyudu@innobuddy.com>
2019-05-09 14:43:39 +0800

    add base files

---
+++ build.sh
@@ -0,0 +1,16 @@
+tags=$(<./tags)
+
+for tag in $tags; do
+    echo FROM node:$tag > ./Dockerfile
+    echo RUN npm config set registry https://registry-npm.smartstudy.com/ >> ./Dockerfile
+    echo USER node >> ./Dockerfile
+    echo "RUN cd /tmp && npm i " $(paste -s -d" "  ./packages) "&& rm -rf /tmp/*" >> ./Dockerfile
+    echo USER root >> ./Dockerfile
+    echo "RUN echo http://mirrors.aliyun.com/alpine/v3.9/main/ > /etc/apk/repositories \
+  && apk update \
+  && apk add tzdata \
+  && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
+  && echo Asia/Shanghai > /etc/timezone" >> ./Dockerfile
+    docker build . -t zhikesmartstudy/node:$tag
+    docker push zhikesmartstudy/node:$tag
+done
